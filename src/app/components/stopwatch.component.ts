74a917b4ac1a267e14e5cf25f78b07050ebf255d
Володя Андрейчук <pow3r@Volodas-MacBook-Air.local>
2022-12-17 23:38:54 +0200

    remove useless variables from stopwatch component

--- src/app/components/stopwatch.component.ts
+++ src/app/components/stopwatch.component.ts
@@ -8,13 +8,11 @@ import { Observable, Subscription, timer, of, delay, tap } from 'rxjs';
   styleUrls: ['./stopwatch.component.scss'],
 })
 export class StopwatchComponent {
-  title: string = 'Stopwatch';
   started: boolean = false;
-  startBtn: string = 'Start';
   clickCount: number = 0;
   // interval: string | number | NodeJS.Timeout | undefined; // it`s variable for no rxjs approach
   timer: Observable<number> = timer(1000, 1000);
-  subscription: Subscription | undefined;
+  subscription?: Subscription;
   time = new Date(0);
 
   // bellow with rxjs & Observable
@@ -25,12 +23,10 @@ export class StopwatchComponent {
       this.subscription = this.timer.subscribe(() => {
         this.time.setSeconds(this.time.getSeconds() + 1);
       });
-      this.startBtn = 'Stop';
     } else {
       this.subscription?.unsubscribe();
       this.time.setSeconds(0);
       this.time.setMinutes(0);
-      this.startBtn = 'Start';
     }
   }
 
@@ -45,7 +41,6 @@ export class StopwatchComponent {
           } else if (this.clickCount === 2 && this.time.getSeconds() >= 1) {
             this.subscription?.unsubscribe();
             this.started = !this.started;
-            this.startBtn = 'Start';
           }
           this.clickCount = 0;
         })
@@ -63,11 +58,9 @@ export class StopwatchComponent {
   //           (this.interval = setInterval(() => {
   //             this.time.setSeconds(this.time.getSeconds() + 1);
   //           }, 1000)),
-  //           (this.startBtn = 'Stop'))
   //         : (this.time.setSeconds(0),
   //           this.time.setMinutes(0),
   //           clearInterval(this.interval),
-  //           (this.startBtn = 'Start'));
   //     }
   //   }
 
@@ -79,7 +72,6 @@ export class StopwatchComponent {
   //       } else if (this.clickCount === 2) {
   //         clearInterval(this.interval);
   //         this.started = !this.started;
-  //         this.startBtn = 'Start';
   //       }
   //       this.clickCount = 0;
   //     }, 500);
